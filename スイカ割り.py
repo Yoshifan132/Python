0abd0102c4bd78ae8eb5aca21eafc2394775a31c
Ryohei Ishii <147536544+Yoshifan132@users.noreply.github.com>
2023-10-12 16:10:23 +0900

    Update スイカ割り.py

--- スイカ割り.py
+++ スイカ割り.py
@@ -12,15 +12,16 @@ def calc_distance(x1, y1, x2, y2):
 suika_x = random.randrange(0, 5)  # スイカのx座標
 suika_y = random.randrange(0, 5)  # スイカのy座標
 
-player_x = 0 # プレイヤーのx座標
-player_y = 0 # プレイヤーのy座標
+player_x =  0# プレイヤーのx座標
+player_y =  0# プレイヤーのy座標
 
 # スイカとプレイヤーの位置が異なる間、処理を繰り返す
 while (suika_x != player_x) or (suika_y != player_y):
 
     # スイカとプレイヤーの距離を表示する
-    distance = calc_distance(player_x, player_y, suika_x, suika_y)
-    print("スイカへの距離:", distance)
+    distance_s = suika_y-player_y
+    distance_e = suika_x-player_x
+    print("スイカへの距離:", distance_s, distance_e)
     
     # キー入力に応じて、プレイヤーを移動する
     c = input("s:南に移動 e:東に移動")
